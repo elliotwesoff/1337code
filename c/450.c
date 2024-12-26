4b0f0e9c53a2e6546dcee6ccef7b3a9fa4d69dd1
Elliot Wesoff <ewesoff@gmail.com>
2024-12-26 11:43:47 +0100

    450 works! ;_;

--- c/450.c
+++ c/450.c
@@ -10,12 +10,15 @@
 
 void printCurrentLevel(struct TreeNode* root, int level);
 int height(struct TreeNode* node);
+struct TreeNode* deleteNode(struct TreeNode* root, int key);
 
 void printLevelOrder(struct TreeNode* root) {
-    int h = height(root);
-    int i;
-    for (i = 1; i <= h; i++)
-        printCurrentLevel(root, i);
+  int h = height(root);
+  int i;
+  for (i = 1; i <= h; i++)
+    printCurrentLevel(root, i);
+
+  printf("\n");
 }
 
 void printCurrentLevel(struct TreeNode* root, int level) {
@@ -44,8 +47,10 @@ int height(struct TreeNode* node) {
     }
 }
 
+/*
+ * deletion of node with two children (case 3)
+ */
 void setup_case1(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5,struct TreeNode* n6) {
-  // deletion of node with two children (case 3)
   n1->val = 5;
   n2->val = 3;
   n3->val = 6;
@@ -69,6 +74,7 @@ void setup_case1(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,str
 void expect_case1(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5,struct TreeNode* n6) {
   assert(n1->left->val == n5->val);
   assert(n1->left->left->val == n4->val);
+  assert(n1->left->right == NULL);
 }
 
 /*
@@ -126,22 +132,90 @@ void expect_case3(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,st
   assert(n1->left->right == NULL);
 }
 
-void delete(struct TreeNode* node, int key) {
+void setup_case4(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5) {
+  n1->val = 5;
+  n2->val = 2;
+  n3->val = 6;
+  n4->val = 4;
+  n5->val = 7;
+  n1->left = n2;
+  n1->right = n3;
+  n2->left = NULL;
+  n2->right = n4;
+  n3->left = NULL;
+  n3->right = n5;
+  n4->left = NULL;
+  n4->right = NULL;
+  n5->left = NULL;
+  n5->right = NULL;
+}
+void expect_case4(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5) {
+  // delete 2
+  assert(n1->left->val == n4->val);
+}
+
+void setup_case5(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5,struct TreeNode* n6) {
+  n1->val = 5;
+  n2->val = 3;
+  n3->val = 6;
+  n4->val = 2;
+  n5->val = 4;
+  n6->val = 7;
+  n1->left = n2;
+  n1->right = n3;
+  n2->left = n4;
+  n2->right = n5;
+  n3->left = NULL;
+  n3->right = n6;
+  n4->left = NULL;
+  n4->right = NULL;
+  n5->left = NULL;
+  n5->right = NULL;
+  n6->left = NULL;
+  n6->right = NULL;
+}
+
+void expect_case5(struct TreeNode* n1,struct TreeNode* n2,struct TreeNode* n3,struct TreeNode* n4,struct TreeNode* n5,struct TreeNode* n6) {
+  assert(n1->val == 6);
+  assert(n1->right->right == NULL);
+}
+
+/*
+ * leftmost leaf node in the right subtree. call this method
+ * with the right of the node, i.e. inorderSuccessor(node->right)
+ */
+struct TreeNode* inorderSuccessor(struct TreeNode* node) {
+  struct TreeNode* n = node;
+  while (n->left != NULL) {
+    n = n->left;
+  }
+  return n;
+}
+
+struct TreeNode* delete(struct TreeNode* node, int key) {
   if (node->left == NULL && node->right == NULL) { // case 1
-    return;
+    return NULL;
   }
 
   if (node->left != NULL && node->right == NULL) { // case 2.1
-    return;
+    return node->left;
   }
 
   if (node->left == NULL && node->right != NULL) { // case 2.2
-    return;
+    return node->right;
   }
   
   if (node->left != NULL && node->right != NULL) { // case 3
-    return;
+    // find inorder successor
+    // overwrite*** the value in the current node with successor->val
+    // use deleteNode(node->right, key) to find and delete the inorder successor
+    struct TreeNode* suc = inorderSuccessor(node->right);
+    deleteNode(node, suc->val);
+    node->val = suc->val;
+    return node;
   }
+
+  return node; // should never be reached
 }
 
 struct TreeNode* deleteNode(struct TreeNode* root, int key) {
@@ -150,11 +224,11 @@ struct TreeNode* deleteNode(struct TreeNode* root, int key) {
   }
 
   if (key < root->val) {
-    deleteNode(root->left, key); // recurse
+    root->left = deleteNode(root->left, key); // recurse
   } else if (key > root->val) {
-    deleteNode(root->right, key); // recurse
+    root->right = deleteNode(root->right, key); // recurse
   } else { // node found
-    delete(root, key); // no recurse, let control go to end of fn
+    return delete(root, key);
   }
 
   return root;
@@ -166,14 +240,32 @@ int main(int argc, char *argv[])
 
   // try to delete node that doesn't exist
   setup_case2(&n1, &n2, &n3, &n4, &n5);
-  deleteNode(&n1, 0);
-  printLevelOrder(&n1);
+  struct TreeNode* root = deleteNode(&n1, 0);
+  printLevelOrder(root);
   expect_case2(&n1, &n2, &n3, &n4, &n5);
 
+  // try to delete node with no children
+  setup_case3(&n1, &n2, &n3, &n4, &n5);
+  root = deleteNode(&n1, 4);
+  printLevelOrder(root);
+  expect_case3(&n1, &n2, &n3, &n4, &n5);
+
+  // try to delete node with one child
+  setup_case4(&n1, &n2, &n3, &n4, &n5);
+  root = deleteNode(&n1, 2);
+  printLevelOrder(root);
+  expect_case4(&n1, &n2, &n3, &n4, &n5);
+
   // delete node with 2 children - case 3
-  // setup_case1(&n1, &n2, &n3, &n4, &n5, &n6);
-  // deleteNode(&n1, 3);
-  // expect_case1(&n1, &n2, &n3, &n4, &n5, &n6);
+  setup_case1(&n1, &n2, &n3, &n4, &n5, &n6);
+  root = deleteNode(&n1, 3);
+  printLevelOrder(root);
+  expect_case1(&n1, &n2, &n3, &n4, &n5, &n6);
+
+  setup_case5(&n1, &n2, &n3, &n4, &n5, &n6);
+  root = deleteNode(&n1, 5);
+  printLevelOrder(root);
+  expect_case5(&n1, &n2, &n3, &n4, &n5, &n6);
 
   return EXIT_SUCCESS;
 }
---
+++ c/gdb/450.gdb
@@ -0,0 +1,15 @@
+display n1
+display n2
+display n3
+display n4
+display n5
+display n6
+
+display &n1
+display &n2
+display &n3
+display &n4
+display &n5
+display &n6
+
+break deleteNode
