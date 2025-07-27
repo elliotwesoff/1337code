aa51d2b3a4a69dbc3efc624247444c6854644839
Elliot Wesoff <ewesoff@gmail.com>
2025-07-27 13:21:13 -0700

    solve problem 217 in rust(!)

---
+++ rust/src/main.rs
@@ -0,0 +1,7 @@
+mod solution_217;
+
+use solution_217::Solution217;
+
+fn main() {
+    Solution217::run();
+}
---
+++ rust/src/solution_217.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+
+pub struct Solution217 {
+}
+
+impl Solution217 {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut hs: HashSet<i32> = HashSet::new();
+
+        for n in nums {
+            if hs.contains(&n) {
+                return true;
+            }
+            hs.insert(n);
+        }
+
+        return false;
+    }
+
+    pub fn run() {
+        println!("*** Running solution 217 ***");
+
+        let input1 = vec![1, 2, 3, 1];
+        let r1 = Solution217::contains_duplicate(input1);
+        assert_eq!(r1, true);
+
+        let input2 = vec![1, 2, 3, 1];
+        let r2 = Solution217::contains_duplicate(input2);
+        assert_eq!(r2, true);
+
+        let input3 = vec![1, 2, 3, 1];
+        let r3 = Solution217::contains_duplicate(input3);
+        assert_eq!(r3, true);
+
+        println!("*** End solution 217 ***");
+    }
+}
